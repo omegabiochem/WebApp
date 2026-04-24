84dfcd35151c47a47e842282c7640161bbe8895a
omegabiochem <omegabiocheminc@gmail.com>
2026-04-24 10:09:03 -0400

    FIX: fixed small issues and also added localstorage for MC dashboard

--- apps/frontend/src/pages/Dashboard/QaDashboard.tsx
+++ apps/frontend/src/pages/Dashboard/QaDashboard.tsx
@@ -690,7 +690,7 @@ export default function QaDashboard() {
   const [selectedIds, setSelectedIds] = useState<string[]>([]);
 
   const PIN_STORAGE_KEY = userKey
-    ? `clientDashboardPinned:user:${userKey}`
+    ? `qaDashboardPinned:user:${userKey}`
     : null;
 
   const [pinnedIds, setPinnedIds] = useState<string[]>([]);
