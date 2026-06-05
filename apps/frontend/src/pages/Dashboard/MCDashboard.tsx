188cffee7ad025dc59da3ca1a6528e626972879a
omegabiochem <omegabiocheminc@gmail.com>
2026-06-05 16:22:15 -0400

    UPDATE: updated missing submission form in bulk printing

--- apps/frontend/src/pages/Dashboard/MCDashboard.tsx
+++ apps/frontend/src/pages/Dashboard/MCDashboard.tsx
@@ -448,6 +448,15 @@ function BulkPrintArea({
       }
     >
       {reports.map((r) => {
+
+
+const paneToPrint =
+  printPane ??
+  (["DRAFT", "UNDER_DRAFT_REVIEW", "SUBMITTED_BY_CLIENT"].includes(
+    String(r.status)
+  )
+    ? "FORM"
+    : "REPORT");
         if (r.kind === "MICRO") {
           if (r.formType === "MICRO_MIX") {
             return (
@@ -458,7 +467,7 @@ function BulkPrintArea({
                   showSwitcher={false}
                   isBulkPrint={true}
                   isSingleBulk={isSingle}
-                  pane={printPane}
+                  pane={paneToPrint}
                 />
               </div>
             );
@@ -472,7 +481,7 @@ function BulkPrintArea({
                   showSwitcher={false}
                   isBulkPrint={true}
                   isSingleBulk={isSingle}
-                  pane={printPane}
+                  pane={paneToPrint}
                 />
               </div>
             );
@@ -486,7 +495,7 @@ function BulkPrintArea({
                   showSwitcher={false}
                   isBulkPrint={true}
                   isSingleBulk={isSingle}
-                  pane={printPane}
+                  pane={paneToPrint}
                 />
               </div>
             );
@@ -509,7 +518,7 @@ function BulkPrintArea({
                 showSwitcher={false}
                 isBulkPrint={true}
                 isSingleBulk={isSingle}
-                pane={printPane}
+                pane={paneToPrint}
               />
             </div>
           );
@@ -525,7 +534,7 @@ function BulkPrintArea({
                 showSwitcher={false}
                 isBulkPrint={true}
                 isSingleBulk={isSingle}
-                pane={printPane}
+                pane={paneToPrint}
               />
             </div>
           );
@@ -2580,7 +2589,7 @@ export default function MCDashboard() {
                     : []
               }
               printPane={
-                isBulkPrinting ? "REPORT" : (singlePrintJob?.pane ?? "REPORT")
+                isBulkPrinting ? undefined : singlePrintJob!.pane
               }
               onAfterPrint={() => {
                 if (isBulkPrinting) setIsBulkPrinting(false);
